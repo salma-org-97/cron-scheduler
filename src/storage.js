7f00cc062fb3721d2382489e7ae893b818e152c7
SalmaZed <salmazaki1997@gmail.com>
2022-12-06 16:35:15 +0200

    chore: update documentation of storage class

--- src/storage.js
+++ src/storage.js
@@ -1,13 +1,26 @@
+/** Class representing the scheduled jobs. */
 class ScheduledJobsMap {
-
+     /**
+     * Creates a map to store the scheduled jobs.
+     */
     constructor() {
         this.map = new Map()
     }
 
+    /**
+     * Adds a job to the map of scheduled jobs with its jobID as the key.
+     * @param  {string} jobID The job ID
+     * @param  {Object} job The job object
+     * @returns
+     */
     save(jobID, job) {
         this.map.set(jobID, job)
     }
 
+    /**
+     * Gets the number of jobs in the map.
+     * @returns
+     */
     getLength() {
         return this.map.size
     }
