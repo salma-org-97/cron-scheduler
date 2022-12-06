ba1821fd8fa28d57f35f25459e53b3d4463f322b
SalmaZed <salmazaki1997@gmail.com>
2022-12-06 13:21:34 +0200

    chore: add documentation & fix logs

--- src/job.js
+++ src/job.js
@@ -1,7 +1,15 @@
-const logger = require('../config/logger')
 const Scheduler = require('./scheduler')
+const logger = require('../config/logger')
 
+/** Class representing a job. */
 class Job {
+    /**
+     * Creates a job.
+     * @param  {string} frequency The scheduling frequency
+     * @param  {string} expectedRunDuration The expected duration of a single job run
+     * @param  {Function} func The function to be executed
+     * @param  {string} jobID The ID of the cronjob
+     */
     constructor({ frequency, expectedRunDuration, func, jobID }) {
         this.frequency = frequency
         this.func = func
@@ -9,21 +17,31 @@ class Job {
         this.expectedRunDuration = expectedRunDuration
     }
 
-    schedule() {
+    /**
+     * Schedules a job according to its scheduling frequency.
+     * @async
+     * @returns
+     */
+    async schedule() {
         this._scheduler = new Scheduler(this.frequency)
-        this._scheduler.on('time-to-execute', () => {
-            this.execute()
+        this._scheduler.on('time-to-execute', async () => {
+            await this.execute()
         })
         this._scheduler.run()
     }
 
-    execute() {
+     /**
+     * Executes the function associated with the job.
+     * @async
+     * @returns
+     */
+    async execute() {
         logger.info(`Starting execution of job ${this.id}`)
-        const startTime = Date.now()
-        this.func()
-        const endTime = Date.now()
-        const executionTime = endTime - startTime
-        logger.info(`Expected run time: ${this.expectedRunDuration}. Actual run time: ${executionTime}`)
+        const startTimeInMillisecs = Date.now()
+        await this.func()
+        const endTimeInMillisecs = Date.now()
+        const executionTime = (endTimeInMillisecs - startTimeInMillisecs) / 1000 + 'sec'
+        logger.info(`Finished execution of job ${this.id}. Expected run duration: ${this.expectedRunDuration}. Actual run duration: ${executionTime}`)
     }
 }
 
--- src/scheduler.js
+++ src/scheduler.js
@@ -1,11 +1,22 @@
 const EventEmitter = require('events')
 
+/** Class representing a job scheduler.
+ *  @extends EventEmitter
+*/
 class Scheduler extends EventEmitter {
+    /**
+     * Creates a job scheduler.
+     * @param  {string} frequency The scheduling frequency
+     */
     constructor(frequency) {
         super()
         this.frequency = frequency
     }
 
+    /**
+     * Runs the scheduler to start scheduling the cronjob.
+     * @returns
+     */
     run() {
         setInterval(() => {
             this.emit('time-to-execute')
